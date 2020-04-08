a94e7fb0a1ac41596d672d7d7c59eb3607014f7a
Louie O Hagan <louieohagan101@gmail.com>
2020-04-08 11:25:11 +0000

    Maint: Added main page routes for starting point to app.py, returning text to ensure correct functionality

--- app.py
+++ app.py
@@ -13,9 +13,44 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello World"
+
+
+@app.route("/sign_up")
+def sign_up():
+    return "Sign Up"
+
+
+@app.route("/sign_in")
+def sign_in():
+    return "Sign In"
+
+
+@app.route("/dashboard")
+def dashboard():
+    return "Dashboard"
+
+
+@app.route("/update_profile")
+def update_profile():
+    return "Update Profile"
+
+
+@app.route("/get_help")
+def get_help():
+    return "Get Help"
+
+
+@app.route("/edit_post")
+def edit_post():
+    return "Edit Post"
+
+
+@app.route("/give_help")
+def give_help():
+    return "Give Help"
 
 
 if __name__ == "__main__":
